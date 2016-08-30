723b01e4f74cafe176a5c6707cc9f082829cbc3e
sri-lab <sri.dev.github.01@gmail.com>
2016-08-30 22:07:07 +0900

    LayerLoader 추가, 기본 기능 처리 완료

--- js/FluxiStore.js
+++ js/FluxiStore.js
@@ -19,9 +19,11 @@ FluxiStore.prototype = {
         //todo : post dispatch
         // - ex) validation check
     },
-    "setInitialData" : function (group, initialData) {
-        initialData = initialData || {};
-        this.data[group] = initialData;
+    "setInitialDataIfEmpty" : function (group, initialData) {
+        if (this.data[group] === undefined) {
+            initialData = initialData || {};
+            this.data[group] = initialData;
+        }
     },
     "save" : function (group, key, val) {
         if (this.data[group] === undefined) {
@@ -60,22 +62,79 @@ FluxiStore.prototype = {
             }
         }
         return true;
-    }
-
-    //resolver : 특정 값을 얻어오기 위한 함수
-    // "addResolver" : function (group, key, func) {
-    //     if (!this.resolvers[group]) {
-    //         this.resolvers[group] = {};
-    //     }
-    //     if (typeof func === "function") {
-    //         this.resolvers[group][key] = func;
+    },
+    //
+    // // resolver : 특정 값을 얻어오기 위한 함수
+    // "addResolver" : function (resolveKey, func) {
+    //     if (typeof func === 'function') {
+    //         this.resolvers[resolveKey] = func;
     //     }
     // },
-    // "resolve" : function (group, key) {
-    //     if (this.resolvers[group] && this.resolvers[group][key]) {
-    //         return this.resolvers[group][key]();
+    // "resolve" : function (resolveKey) {
+    //     if (this.resolvers[resolveKey]) {
+    //         return this.resolvers[resolveKey]();
     //     }
     //     return null;
     // }
+    /**
+     * job_category 데이터를 순회하기
+     * @param callbackFunc
+     * @param excludes
+     * @returns {Array}
+     */
+    "inspectJobCategory" : function (callbackFunc, excludes) {
+        excludes = excludes || [];
+
+        var data = this.data.job_category ? this.data.job_category.job_category : {},
+            contentArr = [],
+            keywordRows;
+
+        for (var code in data) {
+            if (data.hasOwnProperty(code)) {
+                var objByCode = data[code],
+                    codeName = objByCode.name,
+                    keywordsByCode = objByCode.keyword;
+                for (var keyword in keywordsByCode) {
+                    if (keywordsByCode.hasOwnProperty(keyword)) {
+                        if (jQuery.inArray(keyword, excludes) === -1) {
+                            keywordRows = {
+                                'keyword': keyword,
+                                'keywordName': keywordsByCode[keyword],
+                                'codeName': codeName,
+                                'code': code
+                            };
+                            contentArr.push(
+                                callbackFunc ? callbackFunc(keywordRows) : keywordRows
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        return contentArr;
+    },
+    "updateJobCategoryExcludeWith" : function (excludes) {
+        var keywordRows = this.inspectJobCategory(null, excludes),
+            len = keywordRows.length,
+            newJobCategory = {},
+            keyword, keywordName, codeName, code;
+
+        for (var i = 0; i < len; i++) {
+            keyword = keywordRows[i].keyword;
+            keywordName = keywordRows[i].keywordName;
+            codeName = keywordRows[i].codeName;
+            code = keywordRows[i].code;
+
+            if (!newJobCategory[code]) {
+                newJobCategory[code] = {
+                    'name' : codeName,
+                    'keyword' : {}
+                };
+            }
+            newJobCategory[code].keyword[keyword] = keywordName;
+        }
+
+        this.dispatch('job_category', 'job_category', newJobCategory);
+    }
 };
 
---
+++ js/LayerLoader.js
@@ -0,0 +1,42 @@
+/* --- options for jshint --- */
+/*global alert:false*/
+/*global console:false*/
+/*global confirm:false*/
+/*jshint -W100*/
+function LayerLoader(opt) {
+    if (window.jQuery === undefined) {
+        this.log('Check jQuery Loaded!');
+    }
+
+    this.truncateRules = {};
+
+    //±âº» option
+    this.opt = opt || {};
+    this.rootLayerSelector = this.opt.rootLayerSelector || 'body';
+}
+LayerLoader.prototype = {
+    "bindRootClickEvent": function () {
+        var self = this;
+        jQuery(this.rootLayerSelector).on('click', function () {
+            self.truncate();
+        });
+    },
+    "truncate" : function () {
+        var truncateRule;
+
+        for (var i in this.truncateRules) {
+            if (!this.truncateRules.hasOwnProperty(i)) {
+                continue;
+            }
+
+            truncateRule = this.truncateRules[i];
+            truncateRule.function();
+        }
+        this.truncateRules = {};
+    },
+    "addTruncateFuntion": function (truncateRuleObj) {
+        if (!this.truncateRules[truncateRuleObj.id]) {
+            this.truncateRules[truncateRuleObj.id] = truncateRuleObj;
+        }
+    }
+};
